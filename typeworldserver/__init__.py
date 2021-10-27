87b9f8afa0cfb22fb42b86fc2588447ed058fee6
Yanone <post@yanone.de>
2021-10-27 09:43:01 +0200

    Readiness confirmation as local testing startup is sloow

--- typeworldserver/__init__.py
+++ typeworldserver/__init__.py
@@ -613,3 +613,5 @@ if __name__ == "__main__":
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host="0.0.0.0", port=8080, debug=False)
     # [END gae_python37_app]
+
+print("READY...")
